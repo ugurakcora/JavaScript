0d202651f9fe765115a99c13a693a4b6d01ed4bd
ugurakcora <uakcora97@gmail.com>
2019-07-25 14:07:44 +0300

    add

---
+++ aritmetik-operatorler.js
@@ -0,0 +1,28 @@
+//dört işlem
+var deger=12;
+console.log(deger+3);
+var deger=12;
+console.log(deger-3);
+var deger=12;
+console.log(deger*3);
+var deger=12;
+console.log(deger/3);
+
+//bölümümden kalan
+var deger=10;
+console.log(deger%3);
+
+//sayı arttırma
+var deger=10;
+// console.log(deger++); önce yazdırdı değeri arttırdığını görmek için tekrar yazmak zorundasınız. ama;
+console.log(++deger);  //yazarsak önce ekler ve gösterir.
+
+//sayı eksiltme
+    //yukarıda ekleme yaptığımız gibi kullanıyor tek fark + yerine - kullanıyoruz
+var deger=10;
+console.log(--deger);
+
+//10dan 5 çıkar ve yeni sayıya ata
+var sayi=10;
+var yenisayi=sayi-=5;
+console.log(yenisayi);
---
+++ yorum-satiri.js
@@ -0,0 +1,12 @@
+//Tek satırlı yorumlar için
+
+/*
+çok
+satırlı
+yorumlar
+için
+
+ve yorum satırlarını sistem okumaz(yorumlanmaz)
+*/
+var isim="ahmet";
+console.log(isim);
