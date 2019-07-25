6cbd7e91066ad254d0c9875f51e173b0e3656d7e
Uğur Akçora <uakcora97@gmail.com>
2019-07-25 17:20:10 +0300

    added2

---
+++ use-strict.js
@@ -0,0 +1,5 @@
+"use strict";
+data=2;
+console.log(data);
+
+//use strict kullandığımızda strandarta uygun yazarız örneğin var yazmadan da data tanımlanır ama bu hangi versiyonda çalıştırdığını bilmez bundan dolayı starndartlara uygun olarak başta use strict eklenilir.
